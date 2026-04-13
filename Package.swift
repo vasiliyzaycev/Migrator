5b59887ff56d23e48b1a792635feec2d25b12cd3
Vasiliy Zaycev <vasiliy.zaycev@adv.team>
2026-04-13 21:41:03 +0300

    Update platform version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "Migrator",
-  platforms: [.iOS(.v18)],
+  platforms: [.iOS(.v15)],
   products: [
     .library(
       name: "Migrator",
